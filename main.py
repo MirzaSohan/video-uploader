250543a41f121bea494ba414fda1b1e5d47f6a40
Sohan Vau <120249781+MirzaSohan@users.noreply.github.com>
2025-06-11 17:42:00 +0600

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = 'static/videos'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 videos = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        video = request.files['video']
         title = request.form['title']
-        file = request.files['video']
-        if file and title:
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filepath)
-            videos.append({'title': title, 'filename': file.filename})
-            return redirect(url_for('index'))
+        filename = video.filename
+        save_path = os.path.join(UPLOAD_FOLDER, filename)
+        video.save(save_path)
+        videos.append({'title': title, 'filename': filename})
+        return redirect(url_for('index'))
     return render_template('index.html', videos=videos)
 
 if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
